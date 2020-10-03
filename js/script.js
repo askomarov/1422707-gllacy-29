e135364dbcc3b4e3b837a18ccde142006521af92
askomarov <askomarov13@mail.ru>
2020-10-04 01:52:54 +0300

    promoSlider is ready

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 let mySelect = function () {
   let selectHeader = document.querySelectorAll(".select__header");
   let selectItem = document.querySelectorAll(".select__item");
@@ -29,23 +28,44 @@ let mySelect = function () {
       select.classList.remove("is-active");
     }
   });
-
 };
-mySelect();
 
 
 let promoSlider = function () {
   let buttons = document.querySelectorAll(".promo__slider-dot");
   mainPage = document.body;
 
+  let slides = document.querySelectorAll(".slider-item");
+
   for (let i = 0; i <= buttons.length; i++) {
     buttons[i].addEventListener("click", function () {
       mainPage.style.backgroundColor = "var(--special-slide-" + [i] + ")";
-      console.log("нажал кнопку " + [i]);
+      if (i === 0) {
+        buttons[0].classList.add('promo__slider-dot--current');
+        buttons[1].classList.remove('promo__slider-dot--current');
+        buttons[2].classList.remove('promo__slider-dot--current');
+        slides[0].classList.add('slider-item--active');
+        slides[1].classList.remove('slider-item--active');
+        slides[2].classList.remove('slider-item--active');
+      } else if (i === 1) {
+        buttons[1].classList.add('promo__slider-dot--current');
+        buttons[0].classList.remove('promo__slider-dot--current');
+        buttons[2].classList.remove('promo__slider-dot--current');
+        slides[1].classList.add('slider-item--active');
+        slides[0].classList.remove('slider-item--active');
+        slides[2].classList.remove('slider-item--active');
+      } else if (i === 2) {
+        buttons[2].classList.add('promo__slider-dot--current');
+        buttons[0].classList.remove('promo__slider-dot--current');
+        buttons[1].classList.remove('promo__slider-dot--current');
+        slides[2].classList.add('slider-item--active');
+        slides[0].classList.remove('slider-item--active');
+        slides[1].classList.remove('slider-item--active');
+      }
     });
   }
 };
-
 promoSlider();
 
+mySelect();
 
